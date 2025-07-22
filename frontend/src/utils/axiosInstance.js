f4e7025424623b4ff6f745298c0d2e65fa06e6dc
Wasifalikhan064 <wasifalikhan064@gmail.com>
2025-07-22 22:34:16 +0530

    deployment phase

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //import { BASE_URL } from "./apiPaths";
-const BASE_URL = "http://localhost:5000"
+const BASE_URL = "https://expense-tracker-dibq.onrender.com"
 
 const axiosInstance = axios.create({
     baseURL : BASE_URL,
